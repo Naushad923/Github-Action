303bc8060907dd9fc072d6edfcc344625e570a3a
Md Naushad Alam <153056448+Naushad923@users.noreply.github.com>
2026-06-24 22:39:16 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm={
+        source = "hashicorp/azurerm"
+        version = "4.77.0"
+    }
+  }
+}
+provider "azurerm" {
+    features {
+      
+    }
+  
+}
+
+resource "azurerm_resource_group" "rg" {
+  name = "git-nau-rg"
+  location = "westus"
+  
+}
