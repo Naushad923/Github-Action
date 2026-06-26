749a43a3cfce19b047cf71c6ba2b603d8aa8ca73
unknown <naushad231@gmail.com>
2026-06-26 21:34:16 +0530

    add

--- main.tf
+++ main.tf
@@ -18,3 +18,12 @@ resource "azurerm_resource_group" "rg" {
   location = "westus"
   
 }
+
+resource "azurerm_storage_account" "staccount" {
+  name = "naushad15245ba"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+  
+}
